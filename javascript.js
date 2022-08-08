3310207f17f78640e7cb359e608b961067183f73
BRUNO NAOKI NAKAMURA <bruno.naoki.nakamura@gmail.com>
2022-08-08 20:01:46 +0900

    Formulario
    correção de erros
    e adição do formulário na pagina

--- javascript.js
+++ javascript.js
@@ -63,3 +63,65 @@ function reveal() {
         }
     }
 }
+
+// Form
+const form = document.getElementById('form');
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const telefone = document.getElementById('telefone');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const nomeValue = nome.value;
+    const emailValue = email.value;
+    const telefoneValue = telefone.value;
+
+    if (nomeValue === "") {
+        setErrorFor(nome, "Nome obrigatório.");
+    } else {
+        setSuccessFor(nome);
+    }
+
+    if (emailValue === "") {
+        setErrorFor(email, "Email obrigatório.");
+    } else if (!checkEmail(emailValue)) {
+        setErrorFor(email, "Por favor, insira um e-mail válido.");
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (telefoneValue === "") {
+        setErrorFor(telefone, "Telefone obrigatório.");
+    } else {
+        setSuccessFor(telefone);
+    }
+}
+
+function setErrorFor (input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+
+    //adicionar menssagem de erro
+    small.innerText = message;
+
+    //adicionar classe de erro
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor (input) {
+    const formControl = input.parentElement;
+
+    //adicionar a classe de sucesso
+    formControl.className = 'form-control success';
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  }
